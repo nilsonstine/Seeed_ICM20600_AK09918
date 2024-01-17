397374d9545b42c02d18f7e929bf4f8d33777802
nils_kiefer <21311514+nilsonstine@users.noreply.github.com>
2024-01-17 16:54:50 +0100

    WORKS

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,93 @@
 #include <AK09918.h>
 #include <Arduino.h>
 #include <ICM20600.h>
+#include <SimpleKalmanFilter.h>
 #include <Wire.h>
 
-int16_t x, y, z;
-
-int32_t offset_x, offset_y, offset_z;
-double roll, pitch;
+// MAGNOMETER PARAMS (AK09918)
 AK09918 ak09918;
+int32_t x, y, z;
+int32_t offset_x, offset_y, offset_z;
+// ACCELEROMETER PARAMS (ICM20600)
+int16_t acc_x, acc_y, acc_z;
+
+double roll, pitch;
+
+// Find the magnetic declination at your location
+// http://www.magnetic-declination.com/
+double declination = 6.62;
+
 ICM20600 icm20600(true);
 AK09918_err_type_t err;
+
+void calibrate(uint32_t timeout, int32_t *offsetx, int32_t *offsety, int32_t *offsetz) {
+    int32_t value_x_min = 0;
+    int32_t value_x_max = 0;
+    int32_t value_y_min = 0;
+    int32_t value_y_max = 0;
+    int32_t value_z_min = 0;
+    int32_t value_z_max = 0;
+    uint32_t timeStart = 0;
+
+    ak09918.getData(&x, &y, &z);
+
+    value_x_min = x;
+    value_x_max = x;
+    value_y_min = y;
+    value_y_max = y;
+    value_z_min = z;
+    value_z_max = z;
+    delay(100);
+
+    timeStart = millis();
+
+    while ((millis() - timeStart) < timeout) {
+        ak09918.getData(&x, &y, &z);
+
+        /* Update x-Axis max/min value */
+        if (value_x_min > x) {
+            value_x_min = x;
+            Serial.print("Update value_x_min: ");
+            Serial.println(value_x_min);
+
+        } else if (value_x_max < x) {
+            value_x_max = x;
+            Serial.print("update value_x_max: ");
+            Serial.println(value_x_max);
+        }
+
+        /* Update y-Axis max/min value */
+        if (value_y_min > y) {
+            value_y_min = y;
+            Serial.print("Update value_y_min: ");
+            Serial.println(value_y_min);
+
+        } else if (value_y_max < y) {
+            value_y_max = y;
+            Serial.print("update value_y_max: ");
+            Serial.println(value_y_max);
+        }
+
+        /* Update z-Axis max/min value */
+        if (value_z_min > z) {
+            value_z_min = z;
+            Serial.print("Update value_z_min: ");
+            Serial.println(value_z_min);
+
+        } else if (value_z_max < z) {
+            value_z_max = z;
+            Serial.print("update value_z_max: ");
+            Serial.println(value_z_max);
+        }
+
+        Serial.print(".");
+        delay(100);
+    }
+
+    *offsetx = value_x_min + (value_x_max - value_x_min) / 2;
+    *offsety = value_y_min + (value_y_max - value_y_min) / 2;
+    *offsetz = value_z_min + (value_z_max - value_z_min) / 2;
+}
 
 void setup() {
     // join I2C bus (I2Cdev library doesn't do this automatically)
@@ -22,9 +100,10 @@ void setup() {
 
     Serial.println("Starting setup");
 
-    // err = ak09918.initialize();
-    // Serial.println("AK09918 ID: " + String(ak09918.getDeviceID()));
+    err = ak09918.initialize();
+    Serial.println("AK09918 ID: " + String(ak09918.getDeviceID()));
     icm20600.initialize();
+    icm20600.setAccAverageSample(ACC_AVERAGE_4);
     Serial.println("ICM20600 ID: " + String(icm20600.getDeviceID()));
     // Serial.println("err: " + String(err));
     //
@@ -38,14 +117,61 @@ void setup() {
     //      Serial.println("err: " + String(err));
     //  }
 
-    Serial.println("Start figure-8 calibration after 2 seconds.");
-    delay(2000);
-    // calibrate(10000, &offset_x, &offset_y, &offset_z);
-    Serial.println("");
+    Serial.println("Start figure-8");
+    calibrate(10000, &offset_x, &offset_y, &offset_z);
 }
 
 void loop() {
     // get acceleration
-    icm20600.getAcceleration(&x, &y, &z);
-    Serial.println("x: " + String(x) + " y: " + String(y) + " z: " + String(z));
+    acc_x = icm20600.getAccelerationX();
+    acc_y = icm20600.getAccelerationY();
+    acc_z = icm20600.getAccelerationZ();
+
+    Serial.print("A:  ");
+    Serial.print(acc_x);
+    Serial.print(",  ");
+    Serial.print(acc_y);
+    Serial.print(",  ");
+    Serial.print(acc_z);
+    Serial.println(" mg");
+
+    Serial.print("G:  ");
+    Serial.print(icm20600.getGyroscopeX());
+    Serial.print(",  ");
+    Serial.print(icm20600.getGyroscopeY());
+    Serial.print(",  ");
+    Serial.print(icm20600.getGyroscopeZ());
+    Serial.println(" dps");
+
+    ak09918.getData(&x, &y, &z);
+    x = x - offset_x;
+    y = y - offset_y;
+    z = z - offset_z;
+
+    Serial.print("M:  ");
+    Serial.print(x);
+    Serial.print(",  ");
+    Serial.print(y);
+    Serial.print(",  ");
+    Serial.print(z);
+    Serial.println(" uT");
+
+    // roll/pitch in radian
+    roll = atan2((float)acc_y, (float)acc_z);
+    pitch = atan2(-(float)acc_x, sqrt((float)acc_y * acc_y + (float)acc_z * acc_z));
+    Serial.print("Roll: ");
+    Serial.println(roll * 57.3);
+    Serial.print("Pitch: ");
+    Serial.println(pitch * 57.3);
+
+    double Xheading = x * cos(pitch) + y * sin(roll) * sin(pitch) + z * cos(roll) * sin(pitch);
+    double Yheading = y * cos(roll) - z * sin(pitch);
+
+    double heading = 180 + 57.3 * atan2(Yheading, Xheading) + declination;
+
+    Serial.print("Heading: ");
+    Serial.println(heading);
+    Serial.println("--------------------------------");
+
+    delay(200);
 }
